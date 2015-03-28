57e7c33d9a12695add1472af3c89bca060d7c5f5
Leslie Carrier <leslieprince87@gmail.com>
2015-03-28 11:35:53 -0700

    UI fixes and optimizaiton for mobile

--- static/js/default.js
+++ static/js/default.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $("body").keydown(function(e) {
       if(e.which == 37) { // left
 				if($(".prevImg").attr("href")) {
-					window.location.href = $(".prevImg").attr("href")+"/#comic";
+					window.location.href = $(".prevImg").attr("href");
 				}
       }
       else if(e.which == 39) { // right
 				if($(".nextImg").attr("href")) {
-					window.location.href = $(".nextImg").attr("href") + "/#comic";
+					window.location.href = $(".nextImg").attr("href");
 				}
       }
     });
